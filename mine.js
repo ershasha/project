0ca48a27f30d36eb3bd5b71626fa52720e514910
ershasha <1205630593@qq.com>
2019-05-08 10:52:38 +0800

    修改3

--- mine.js
+++ mine.js
@@ -10,12 +10,6 @@ exports.types = {
     "pdf": "application/pdf",
     "png": "image/jpg",
     "svg": "image/svg+xml",
-    "swf": "application/x-shockwave-flash",
-    "tiff": "image/tiff",
-    "txt": "text/plain",
-    "wav": "audio/x-wav",
-    "wma": "audio/x-ms-wma",
-    "wmv": "video/x-ms-wmv",
-    "xml": "text/xml",
+
 };
 
